3bb1802474c7122a093b960a38b4254934d32ced
Daniil <dadyarri@gmail.com>
2023-07-06 16:05:13 +0300

    feat(app): add bottom app bar

---
+++ app/src/main/java/ru/dadyarri/choco/components/navigation/AppBar.kt
@@ -0,0 +1,50 @@
+package ru.dadyarri.choco.components.navigation
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    val navController = rememberNavController()
+    val navItems = listOf(Screen.Home, Screen.Orders, Screen.Shipments, Screen.Profile)
+
+    Scaffold(bottomBar = {
+        NavigationBar {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+            navItems.forEach { item ->
+                NavigationBarItem(icon = { Icon(item.icon, contentDescription = null) }, label = {
+                    Text(
+                        text = stringResource(item.labelId)
+                    )
+                },
+                    selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    })
+            }
+        }
+
+    }) { innerPadding ->
+        AppNavHost(navController = navController, innerPadding = innerPadding)
+    }
+
+}
