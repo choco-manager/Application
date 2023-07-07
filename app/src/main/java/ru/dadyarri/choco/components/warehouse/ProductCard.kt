40ff9c1ca9440e52de60117d1956f71c7de1cae6
Daniil <dadyarri@gmail.com>
2023-07-07 22:30:29 +0300

    refactor(app): use list items in warehouse page

--- app/src/main/java/ru/dadyarri/choco/components/warehouse/ProductCard.kt
+++ app/src/main/java/ru/dadyarri/choco/components/warehouse/ProductCard.kt
@@ -1,14 +1,10 @@
 package ru.dadyarri.choco.components.warehouse
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import ru.dadyarri.choco.entities.Product
 
 @Composable
@@ -17,24 +13,21 @@ fun ProductCard(product: Product, isWholesalePricesEnabled: Boolean) {
     val unit = if (product.isByWeight) "кг" else "шт."
 
     Column {
-        Row(
-            Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column {
-                Text(text = product.title, style = MaterialTheme.typography.headlineSmall)
-
+        ListItem(
+            headlineContent = { Text(text = product.title) },
+            supportingContent = {
                 if (isWholesalePricesEnabled) {
                     Text(text = "${product.wholesalePrice}₽ / ${product.retailPrice}₽")
                 } else {
                     Text(text = "${product.retailPrice}₽")
                 }
+            },
+            trailingContent = {
+                Text(
+                    text = if (product.leftover * 10 % 10 == 0.0) "${product.leftover.toInt()} $unit" else "${product.leftover} $unit",
+                    style = MaterialTheme.typography.labelMedium,
+                )
             }
-            Text(
-                text = if (product.leftover * 10 % 10 == 0.0) "${product.leftover.toInt()} $unit" else "${product.leftover} $unit",
-                style = MaterialTheme.typography.labelMedium,
-            )
-        }
+        )
     }
 }
