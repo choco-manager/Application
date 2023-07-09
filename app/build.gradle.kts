5707dc9bbdc011c649d36a85b731a57770a53322
Daniil <dadyarri@gmail.com>
2023-07-09 11:38:10 +0300

    feat(deps): add dependency for material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,8 +89,11 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
 
     // DataStore
     implementation("androidx.datastore:datastore:1.0.0")
+
+    // Material Design
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended-android:1.5.0-beta03")
 }
