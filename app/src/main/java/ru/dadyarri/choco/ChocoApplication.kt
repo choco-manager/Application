59c7622f6719f5cff420866eeff38beaa491ab44
Daniil <dadyarri@gmail.com>
2023-07-09 13:05:34 +0300

    feat(deps): add base class for application

---
+++ app/src/main/java/ru/dadyarri/choco/ChocoApplication.kt
@@ -0,0 +1,13 @@
+package ru.dadyarri.choco
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChocoApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
