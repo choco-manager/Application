0c258a0b8127005f027ce48f291ba76d7cd46444
Daniil <dadyarri@gmail.com>
2023-07-12 02:53:06 +0300

    feat(app): add ui to select application theme

--- app/src/main/java/ru/dadyarri/choco/MainActivity.kt
+++ app/src/main/java/ru/dadyarri/choco/MainActivity.kt
@@ -4,18 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ChevronRight
+import androidx.compose.material.icons.rounded.LightMode
+import androidx.compose.material.icons.rounded.ModeNight
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -39,6 +53,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             val systemUiController = rememberSystemUiController()
             val prefs = viewModel.getPreferences().collectAsState().value
+            var themeDialogShowed by remember { mutableStateOf(false) }
+            var darkThemeSelected by remember { mutableStateOf(prefs.darkThemeConfig == DarkThemeConfig.DARK) }
+            var lightThemeSelected by remember { mutableStateOf(prefs.darkThemeConfig == DarkThemeConfig.LIGHT) }
+            var systemThemeSelected by remember { mutableStateOf(prefs.darkThemeConfig == DarkThemeConfig.FOLLOW_SYSTEM) }
 
             ChocoManagerTheme {
                 // A surface container using the 'background' color from the theme
@@ -52,20 +70,93 @@ class MainActivity : ComponentActivity() {
                         color = MaterialTheme.colorScheme.background
                     ) {
                         Column {
-                            Greeting(
-                                prefs.darkThemeConfig.name
-                            )
-                            Button(onClick = { viewModel.updateDarkThemeConfig(DarkThemeConfig.LIGHT) }) {
-                                Text(text = "LIGHT")
-                            }
-                            Button(onClick = { viewModel.updateDarkThemeConfig(DarkThemeConfig.DARK) }) {
-                                Text(text = "DARK")
-                            }
-                            Button(onClick = { viewModel.updateDarkThemeConfig(DarkThemeConfig.FOLLOW_SYSTEM) }) {
-                                Text(text = "SYSTEM")
-                            }
+                            ListItem(
+                                modifier = Modifier.clickable(onClick = {
+                                    themeDialogShowed = true
+                                }),
+                                headlineText = { Text(text = "Тема приложения") },
+                                trailingContent = {
+                                    Icon(
+                                        imageVector = Icons.Rounded.ChevronRight,
+                                        contentDescription = null
+                                    )
+                                })
                         }
                     }
+                }
+                if (themeDialogShowed) {
+                    AlertDialog(
+                        onDismissRequest = { themeDialogShowed = false },
+                        title = {
+                            Text(
+                                text = "Настройки темы",
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        },
+                        text = {
+                            Column {
+                                ListItem(
+                                    leadingContent = {
+                                        Icon(
+                                            imageVector = Icons.Rounded.ModeNight,
+                                            contentDescription = null
+                                        )
+                                    },
+                                    headlineText = { Text(text = "Тёмная") },
+                                    trailingContent = {
+                                        RadioButton(
+                                            selected = darkThemeSelected,
+                                            onClick = {
+                                                darkThemeSelected = true
+                                                lightThemeSelected = false
+                                                systemThemeSelected = false
+                                                viewModel.updateDarkThemeConfig(DarkThemeConfig.DARK)
+                                            })
+                                    }
+                                )
+                                ListItem(
+                                    leadingContent = {
+                                        Icon(
+                                            imageVector = Icons.Rounded.LightMode,
+                                            contentDescription = null
+                                        )
+                                    },
+                                    headlineText = { Text(text = "Светлая") },
+                                    trailingContent = {
+                                        RadioButton(
+                                            selected = lightThemeSelected,
+                                            onClick = {
+                                                lightThemeSelected = true
+                                                darkThemeSelected = false
+                                                systemThemeSelected = false
+                                                viewModel.updateDarkThemeConfig(DarkThemeConfig.LIGHT)
+                                            })
+                                    }
+                                )
+                                ListItem(
+                                    leadingContent = { androidx.compose.material3.Icon(
+                                        imageVector = Icons.Rounded.Settings,
+                                        contentDescription = null
+                                    )},
+                                    headlineText = { Text(text = "Как в системе") },
+                                    trailingContent = {
+                                        RadioButton(
+                                            selected = systemThemeSelected,
+                                            onClick = {
+                                                systemThemeSelected = true
+                                                darkThemeSelected = false
+                                                lightThemeSelected = false
+                                                viewModel.updateDarkThemeConfig(DarkThemeConfig.FOLLOW_SYSTEM)
+                                            })
+                                    }
+                                )
+                            }
+                        },
+                        confirmButton = {
+                            TextButton(onClick = { themeDialogShowed = false }) {
+                                Text(text = "Закрыть")
+                            }
+                        })
                 }
             }
         }
