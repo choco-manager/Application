1deb80dd3e90928ee3e7b21c9589a3a4b9b50bb1
Daniil <dadyarri@gmail.com>
2023-07-12 02:09:35 +0300

    feat(app): add dependency injection mechanism

---
+++ app/src/main/java/ru/dadyarri/choco/core/di/DependencyInjection.kt
@@ -0,0 +1,31 @@
+package ru.dadyarri.choco.core.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DependencyInjection {
+
+    @Provides
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(produceNewData = { emptyPreferences() }),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = {context.preferencesDataStoreFile("user_preferences")}
+        )
+    }
+}
