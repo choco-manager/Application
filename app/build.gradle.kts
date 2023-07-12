86b4041a26fd493f569e8843da1384269c7e8cea
Daniil <dadyarri@gmail.com>
2023-07-12 19:47:48 +0300

    feat(deps): add dependency to implement navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
