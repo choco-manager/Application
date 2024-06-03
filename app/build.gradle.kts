d163cd1a1cc8487f92f94e81113a6f710edcecba
Daniil <dadyarri@gmail.com>
2024-06-03 22:59:05 +0300

    chore(release): 3.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "ru.dadyarri.choco"
         minSdk = 29
         targetSdk = 34
-        versionName = "3.1.5"
+        versionName = "3.1.6"
 
         vectorDrawables {
             useSupportLibrary = true
