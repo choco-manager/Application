52a2fafbe49a1d85588e603a89d5950b85002b45
Daniil <dadyarri@gmail.com>
2024-06-04 17:17:40 +0300

    feat: add top and bottom bar

---
+++ app/src/main/java/ru/dadyarri/choco/ui/components/common/BottomBar.kt
@@ -0,0 +1,43 @@
+package ru.dadyarri.choco.ui.components.common
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.dadyarri.choco.navigation.screen.Screen
+import ru.dadyarri.choco.navigation.screen.ScreenPosition
+import ru.dadyarri.choco.util.navigate
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = backStackEntry?.destination
+    NavigationBar {
+        enumValues<Screen>().filter { screen -> screen.icon != null && screen.config.position == ScreenPosition.Bottom }
+            .forEach { screen ->
+                val isSelected =
+                    currentDestination?.hierarchy?.any { it.route == screen.name } == true
+
+                NavigationBarItem(
+                    selected = isSelected,
+                    onClick = { navigate(navController, screen.route, saveEntry = false) },
+                    icon = {
+                        if (screen.icon != null) {
+                            Icon(
+                                screen.icon,
+                                null,
+                                tint = if (isSelected) MaterialTheme.colorScheme.onSecondaryContainer else MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
+                )
+            }
+    }
+}
---
+++ app/src/main/java/ru/dadyarri/choco/ui/components/common/TopBar.kt
@@ -0,0 +1,45 @@
+package ru.dadyarri.choco.ui.components.common
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    canGoBack: Boolean,
+    goBack: () -> Boolean,
+    modifier: Modifier = Modifier,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(title, style = MaterialTheme.typography.titleLarge)
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canGoBack) {
+                IconButton(onClick = { goBack() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
+        actions = actions
+    )
+}
