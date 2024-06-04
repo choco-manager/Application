b6e8687dce53f61ca95acdf3002d6b9ae8692f38
Daniil <dadyarri@gmail.com>
2024-06-04 17:26:07 +0300

    refactor: remove missed unused stuff

--- app/src/main/java/ru/dadyarri/choco/ui/screens/ChocoApp.kt
+++ app/src/main/java/ru/dadyarri/choco/ui/screens/ChocoApp.kt
@@ -3,23 +3,14 @@ package ru.dadyarri.choco.ui.screens
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ru.dadyarri.choco.navigation.routes.Route
-import ru.dadyarri.choco.navigation.screen.ScreenConfig
 import ru.dadyarri.choco.system.navigation.NavigationHandler
 import ru.dadyarri.choco.system.snackbar.SnackbarMessageHandler
-import ru.dadyarri.choco.ui.components.Greeting
-import ru.dadyarri.choco.viewmodels.LoginViewModel
-import ru.dadyarri.choco.viewmodels.RestorePasswordViewModel
 
 @Composable
 fun ChocoApp(
