01d6afa33390412fb7b15f6a2155032798bb12f8
Daniil <dadyarri@gmail.com>
2024-06-04 17:21:45 +0300

    feat: add some more routes

--- app/src/main/java/ru/dadyarri/choco/navigation/routes/routes.kt
+++ app/src/main/java/ru/dadyarri/choco/navigation/routes/routes.kt
@@ -7,4 +7,10 @@ sealed class Route {
     data object Login: Route()
     @Serializable
     data object RestorePassword: Route()
+    @Serializable
+    data object Orders: Route()
+    @Serializable
+    data object Procurements: Route()
+    @Serializable
+    data object Menu: Route()
 }
