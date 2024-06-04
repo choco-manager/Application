bbc975c1e52712d377e790a21b77d38cf56f22eb
Daniil <dadyarri@gmail.com>
2024-06-04 09:21:39 +0300

    feat: add base screen

---
+++ app/src/main/java/ru/dadyarri/choco/ui/screens/BaseScreen.kt
@@ -0,0 +1,74 @@
+package ru.dadyarri.choco.ui.screens
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BaseScreen(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    children: @Composable () -> Unit
+) {
+
+    Column(
+        Modifier
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = FastOutSlowInEasing,
+                    delayMillis = 500
+                )
+            )
+    ) {
+//        if (!LocalInspectionMode.current) {
+//            val viewModel: TopBarViewModel = hiltViewModel()
+//            val networkState by
+//            viewModel.networkState.collectAsState(ConnectivityObserver.Status.Available)
+//
+//            AnimatedVisibility(
+//                visible = networkState != ConnectivityObserver.Status.Available,
+//                enter = fadeIn() + slideInVertically(
+//                    initialOffsetY = { fullHeight -> -fullHeight },
+//                    animationSpec = tween(durationMillis = 500, easing = FastOutSlowInEasing)
+//                ),
+//                exit = fadeOut() + slideOutVertically(
+//                    targetOffsetY = { fullHeight -> -fullHeight },
+//                    animationSpec = tween(durationMillis = 500, easing = FastOutSlowInEasing)
+//                )
+//            ) {
+//                NoInternetAvailable()
+//            }
+//        }
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(28.dp)
+        ) {
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = horizontalAlignment,
+                verticalArrangement = verticalArrangement
+            ) {
+                children()
+            }
+        }
+    }
+}
