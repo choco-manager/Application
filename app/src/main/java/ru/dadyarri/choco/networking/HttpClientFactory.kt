8edf782a6aae7c82a14a3e4d45109ab07218524c
Daniil <dadyarri@gmail.com>
2024-06-07 19:06:11 +0300

    feat: add http client factory

---
+++ app/src/main/java/ru/dadyarri/choco/networking/HttpClientFactory.kt
@@ -0,0 +1,28 @@
+package ru.dadyarri.choco.networking
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+
+object HttpClientFactory {
+    const val baseUrl = ""
+    val httpClient: HttpClient by lazy {
+        HttpClient(Android) {
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+
+            install(ContentNegotiation) {
+                json()
+            }
+
+            install(DefaultRequest) {
+                url(scheme = "https", host = baseUrl)
+            }
+        }
+    }
+}
