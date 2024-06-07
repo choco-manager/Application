85f22e7a804a8d2a94b1c4342d63b609858660bc
Daniil <dadyarri@gmail.com>
2024-06-07 19:49:04 +0300

    feat: add methods to retrieve tokens from storage

--- app/src/main/java/ru/dadyarri/choco/storage/DataStoreManager.kt
+++ app/src/main/java/ru/dadyarri/choco/storage/DataStoreManager.kt
@@ -2,11 +2,20 @@ package ru.dadyarri.choco.storage
 
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DataStoreManager @Inject constructor(@ApplicationContext private val context: Context) {
+
+    suspend fun getAccessToken(): String {
+        return context.dataStore.data.first().accessToken
+    }
+
+    suspend fun getRefreshToken(): String {
+        return context.dataStore.data.first().refreshToken
+    }
 
     suspend fun updateAccessToken(accessToken: String) {
         context.dataStore.updateData {
