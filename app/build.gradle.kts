cf0a5c1db8d86aa537c701a05a679e759772b6ce
Daniil <dadyarri@gmail.com>
2024-06-07 23:19:19 +0300

    Revert "chore(release): 3.3.0"
    This reverts commit d3eb5b76c58c34b96f6eba3ac73b145be09c1ddd.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "ru.dadyarri.choco"
         minSdk = 29
         targetSdk = 34
-        versionName = "3.3.0"
+        versionName = "3.2.0"
 
         vectorDrawables {
             useSupportLibrary = true
