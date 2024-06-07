9ac36b434d37e3df320ee5aba1000513479544cb
Daniil <dadyarri@gmail.com>
2024-06-07 19:07:18 +0300

    refactor: rename class; remove data from loading state

--- app/src/main/java/ru/dadyarri/choco/common/Response.kt
+++
@@ -1,7 +0,0 @@
-package ru.dadyarri.choco.common
-
-sealed class Response<T>(val data: T? = null, val message: UiText? = null) {
-    class Success<T>(data: T) : Response<T>(data)
-    class Error<T>(message: UiText, data: T? = null) : Response<T>(data, message)
-    class Loading<T>(data: T? = null) : Response<T>(data)
-}
