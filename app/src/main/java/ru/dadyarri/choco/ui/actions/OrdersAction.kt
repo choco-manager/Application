8b257b025e369a928a2ac0796cd29db6cc211de8
Daniil <dadyarri@gmail.com>
2024-06-08 17:12:33 +0300

    feat: add action for fab on orders screen

--- app/src/main/java/ru/dadyarri/choco/ui/actions/OrdersAction.kt
+++ app/src/main/java/ru/dadyarri/choco/ui/actions/OrdersAction.kt
@@ -5,4 +5,5 @@ import java.util.UUID
 sealed class OrdersAction {
     data object Refresh : OrdersAction()
     data class OpenOrder(val id: UUID) : OrdersAction()
+    data object Create : OrdersAction()
 }
