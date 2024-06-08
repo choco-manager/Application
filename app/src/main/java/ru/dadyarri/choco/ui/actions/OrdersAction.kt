1561ee2f545e40d25de3e2196b1883319fd91bf4
Daniil <dadyarri@gmail.com>
2024-06-08 14:58:45 +0300

    fix: update some stuff

--- app/src/main/java/ru/dadyarri/choco/ui/actions/OrdersAction.kt
+++ app/src/main/java/ru/dadyarri/choco/ui/actions/OrdersAction.kt
@@ -3,5 +3,6 @@ package ru.dadyarri.choco.ui.actions
 import java.util.UUID
 
 sealed class OrdersAction {
+    data object Refresh : OrdersAction()
     data class OpenOrder(val id: UUID) : OrdersAction()
 }
--- app/src/main/java/ru/dadyarri/choco/ui/state/OrdersState.kt
+++ app/src/main/java/ru/dadyarri/choco/ui/state/OrdersState.kt
@@ -1,9 +1,11 @@
 package ru.dadyarri.choco.ui.state
 
 import ru.dadyarri.choco.R
+import ru.dadyarri.choco.common.Resource
 import ru.dadyarri.choco.common.UiText
+import ru.dadyarri.choco.domain.orders.data.ListOfOrders
 
 data class OrdersState(
     val title: UiText = UiText.StringResource(R.string.orders_screen),
-    val isRefreshing: Boolean = false,
+    val data: Resource<ListOfOrders> = Resource.Loading(),
 )
