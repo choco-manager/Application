57a9fde3a1d4b18eed6e34ab106ac3e312e88c05
Daniil <dadyarri@gmail.com>
2024-06-08 17:12:13 +0300

    feat: add some routes

--- app/src/main/java/ru/dadyarri/choco/navigation/routes/routes.kt
+++ app/src/main/java/ru/dadyarri/choco/navigation/routes/routes.kt
@@ -1,19 +1,31 @@
 package ru.dadyarri.choco.navigation.routes
 
 import kotlinx.serialization.Serializable
+import ru.dadyarri.choco.serialization.UuidSerializer
+import java.util.UUID
 
 sealed class Route {
     @Serializable
-    data object Login: Route()
+    data object Login : Route()
+
     @Serializable
     data object RequestPasswordRestoration : Route()
+
     @Serializable
     data object RestorePassword : Route()
 
     @Serializable
-    data object Orders: Route()
+    data object Orders : Route()
+
     @Serializable
-    data object Procurements: Route()
+    data object CreateOrder : Route()
+
     @Serializable
-    data object Menu: Route()
+    data class Order(@Serializable(UuidSerializer::class) val id: UUID) : Route()
+
+    @Serializable
+    data object Procurements : Route()
+
+    @Serializable
+    data object Menu : Route()
 }
