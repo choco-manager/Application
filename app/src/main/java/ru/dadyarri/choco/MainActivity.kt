55303cd745d27ca49fc3590ae7ed54b11dfc8d53
Daniil <dadyarri@gmail.com>
2024-06-09 15:21:39 +0300

    feat: add showing splashscreen

--- app/src/main/java/ru/dadyarri/choco/MainActivity.kt
+++ app/src/main/java/ru/dadyarri/choco/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import ru.dadyarri.choco.system.navigation.NavigationHandler
 import ru.dadyarri.choco.system.snackbar.SnackbarMessageHandler
 import ru.dadyarri.choco.ui.screens.ChocoApp
 import ru.dadyarri.choco.ui.theme.ChocoManagerTheme
+import ru.dadyarri.choco.viewmodels.SplashScreenViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -20,8 +23,16 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var navigationHandler: NavigationHandler
 
+    private val viewModel by viewModels<SplashScreenViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+
+            setKeepOnScreenCondition {
+                viewModel.state.value.isVisible
+            }
+        }
         enableEdgeToEdge()
         setContent {
             ChocoManagerTheme {
