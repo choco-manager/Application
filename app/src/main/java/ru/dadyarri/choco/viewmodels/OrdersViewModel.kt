2fef07f22923bb0282c6b3e81341aff0dd62e668
Daniil <dadyarri@gmail.com>
2024-06-13 16:32:58 +0300

    feat: add actual requesting orders

--- app/src/main/java/ru/dadyarri/choco/viewmodels/OrdersViewModel.kt
+++ app/src/main/java/ru/dadyarri/choco/viewmodels/OrdersViewModel.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import ru.dadyarri.choco.common.Resource
+import ru.dadyarri.choco.domain.orders.services.OrdersService
 import ru.dadyarri.choco.navigation.routes.Route
 import ru.dadyarri.choco.system.navigation.NavigationHandler
 import ru.dadyarri.choco.ui.actions.OrdersAction
@@ -16,10 +19,15 @@ import javax.inject.Inject
 @HiltViewModel
 class OrdersViewModel @Inject constructor(
     private val navigationHandler: NavigationHandler,
+    private val ordersService: OrdersService,
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(OrdersState())
     val state = _state.asStateFlow()
+
+    init {
+        onRefresh()
+    }
 
     fun onAction(action: OrdersAction) {
         when (action) {
@@ -36,7 +44,17 @@ class OrdersViewModel @Inject constructor(
     }
 
     private fun onRefresh() {
+        viewModelScope.launch {
+            val response = ordersService.getAllOrders()
 
+            if (response is Resource.Success) {
+                _state.update {
+                    it.copy(
+                        data = response
+                    )
+                }
+            }
+        }
     }
 
     private fun onCreate() {
