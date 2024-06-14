a945a04370b33e2d0b08e76f79d43a7a12631abb
Daniil <dadyarri@gmail.com>
2024-06-14 22:37:47 +0300

    fix: bodies for some requests

--- app/src/main/java/ru/dadyarri/choco/domain/orders/data/UpdatePaymentStatusRequest.kt
+++ app/src/main/java/ru/dadyarri/choco/domain/orders/data/UpdatePaymentStatusRequest.kt
@@ -1,12 +1,8 @@
 package ru.dadyarri.choco.domain.orders.data
 
 import kotlinx.serialization.Serializable
-import ru.dadyarri.choco.serialization.UuidSerializer
-import java.util.UUID
 
 @Serializable
 data class UpdatePaymentStatusRequest(
-    @Serializable(UuidSerializer::class)
-    val id: UUID,
     val status: PaymentStatus,
 )
